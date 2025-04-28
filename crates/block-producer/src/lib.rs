1371cf2d37a313c60d95759f1a0c100afc1d8504
Tomas <tomas.arrachea@lambdaclass.com>
2025-04-28 20:05:34 -0300

    feat: Component startups with lazy connection (#785)

--- crates/block-producer/src/lib.rs
+++ crates/block-producer/src/lib.rs
@@ -1,3 +1,4 @@
+#![recursion_limit = "256"]
 use std::num::NonZeroUsize;
 
 #[cfg(test)]
@@ -7,6 +8,7 @@ mod batch_builder;
 mod block_builder;
 mod domain;
 mod mempool;
+mod server;
 pub mod store;
 
 #[cfg(feature = "testing")]
@@ -15,6 +17,7 @@ pub mod errors;
 mod errors;
 
 pub mod server;
+pub use server::BlockProducer;
 
 // CONSTANTS
 // =================================================================================================

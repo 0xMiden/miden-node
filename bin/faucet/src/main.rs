3a7081892121229c455870b84d259a204b96b86f
tomasarrachea <tomas.arrachea@lambdaclass.com>
2025-05-27 11:23:06 -0300

    test: single fantoccini client

--- bin/faucet/src/main.rs
+++ bin/faucet/src/main.rs
@@ -290,7 +290,7 @@ mod test {
     use base64::{Engine, prelude::BASE64_STANDARD};
     use fantoccini::ClientBuilder;
     use serde_json::{Map, json};
-    use tokio::{io::AsyncBufReadExt, task::JoinSet, time::sleep};
+    use tokio::{io::AsyncBufReadExt, time::sleep};
     use url::Url;
 
     use crate::{
@@ -304,23 +304,16 @@ mod test {
     #[tokio::test]
     async fn test_website() {
         let website_url = start_test_faucet().await;
-        let chromedriver_port = start_chromedriver().await;
-        let mut join_set = JoinSet::new();
-        for i in 0..1 {
-            let website_url = website_url.clone();
-            let chromedriver_port = chromedriver_port.clone();
-            join_set.spawn(async move {
-                let client = start_fantoccini_client(chromedriver_port).await;
+        let client = start_fantoccini_client().await;
 
-                // Open the website
-                client.goto(website_url.as_str()).await.unwrap();
+        // Open the website
+        client.goto(website_url.as_str()).await.unwrap();
 
-                let title = client.title().await.unwrap();
-                assert_eq!(title, "Miden Faucet");
-                dbg!(i);
+        let title = client.title().await.unwrap();
+        assert_eq!(title, "Miden Faucet");
 
-                // Execute a script to get all the failed requests
-                let script = r"
+        // Execute a script to get all the failed requests
+        let script = r"
             let errors = [];
             performance.getEntriesByType('resource').forEach(entry => {
                 if (entry.responseStatus && entry.responseStatus >= 400) {
@@ -328,48 +321,48 @@ mod test {
                 }
             });
             return errors;
-            ";
-                let failed_requests = client.execute(script, vec![]).await.unwrap();
+        ";
+        let failed_requests = client.execute(script, vec![]).await.unwrap();
 
-                // Verify all requests are successful
-                assert!(failed_requests.as_array().unwrap().is_empty());
+        // Verify all requests are successful
+        assert!(failed_requests.as_array().unwrap().is_empty());
 
-                // Fill in the account address
-                client
-                    .find(fantoccini::Locator::Css("#account-address"))
-                    .await
-                    .unwrap()
-                    .send_keys("mtst1qrvhealccdyj7gqqqrlxl4n4f53uxwaw")
-                    .await
-                    .unwrap();
+        // Fill in the account address
+        client
+            .find(fantoccini::Locator::Css("#account-address"))
+            .await
+            .unwrap()
+            .send_keys("mtst1qrvhealccdyj7gqqqrlxl4n4f53uxwaw")
+            .await
+            .unwrap();
 
-                // Select the first asset amount option
-                client
-                    .find(fantoccini::Locator::Css("#asset-amount"))
-                    .await
-                    .unwrap()
-                    .click()
-                    .await
-                    .unwrap();
-                client
-                    .find(fantoccini::Locator::Css("#asset-amount option"))
-                    .await
-                    .unwrap()
-                    .click()
-                    .await
-                    .unwrap();
+        // Select the first asset amount option
+        client
+            .find(fantoccini::Locator::Css("#asset-amount"))
+            .await
+            .unwrap()
+            .click()
+            .await
+            .unwrap();
+        client
+            .find(fantoccini::Locator::Css("#asset-amount option"))
+            .await
+            .unwrap()
+            .click()
+            .await
+            .unwrap();
 
-                // Click the public note button
-                client
-                    .find(fantoccini::Locator::Css("#button-public"))
-                    .await
-                    .unwrap()
-                    .click()
-                    .await
-                    .unwrap();
+        // Click the public note button
+        client
+            .find(fantoccini::Locator::Css("#button-public"))
+            .await
+            .unwrap()
+            .click()
+            .await
+            .unwrap();
 
-                // Inject JavaScript to capture sse events
-                let capture_events_script = r"
+        // Inject JavaScript to capture sse events
+        let capture_events_script = r"
             window.capturedEvents = [];
             const original = EventSource.prototype.addEventListener;
             EventSource.prototype.addEventListener = function(type, listener) {
@@ -382,46 +375,40 @@ mod test {
                 };
                 return original.call(this, type, wrappedListener);
             };
-            ";
-                client.execute(capture_events_script, vec![]).await.unwrap();
+        ";
+        client.execute(capture_events_script, vec![]).await.unwrap();
 
-                // Poll until minting is complete. We wait 10s and then poll every 2s for a max of
-                // 25 times.
-                sleep(Duration::from_secs(10)).await;
-                dbg!("polling for events");
-                let mut captured_events: Vec<serde_json::Value> = vec![];
-                for _ in 0..25 {
-                    let events = client
-                        .execute("return window.capturedEvents;", vec![])
-                        .await
-                        .unwrap()
-                        .as_array()
-                        .unwrap()
-                        .clone();
-                    if events.iter().any(|event| event["type"] == "note") {
-                        captured_events = events;
-                        break;
-                    }
-                    dbg!("poll");
-                    sleep(Duration::from_secs(2)).await;
-                }
-
-                // Verify the received events
-                assert!(!captured_events.is_empty(), "Took too long to capture any events");
-                assert!(captured_events.iter().any(|event| event["type"] == "update"));
-                let note_event =
-                    captured_events.iter().find(|event| event["type"] == "note").unwrap();
-                let note_data: serde_json::Value =
-                    serde_json::from_str(note_event["data"].as_str().unwrap()).unwrap();
-                assert!(note_data["note_id"].is_string());
-                assert!(note_data["account_id"].is_string());
-                assert!(note_data["transaction_id"].is_string());
-                assert!(note_data["explorer_url"].is_string());
-
-                client.close().await.unwrap();
-            });
+        // Poll until minting is complete. We wait 10s and then poll every 2s for a max of
+        // 25 times.
+        sleep(Duration::from_secs(10)).await;
+        let mut captured_events: Vec<serde_json::Value> = vec![];
+        for _ in 0..25 {
+            let events = client
+                .execute("return window.capturedEvents;", vec![])
+                .await
+                .unwrap()
+                .as_array()
+                .unwrap()
+                .clone();
+            if events.iter().any(|event| event["type"] == "note") {
+                captured_events = events;
+                break;
+            }
+            sleep(Duration::from_secs(2)).await;
         }
-        join_set.join_all().await;
+
+        // Verify the received events
+        assert!(!captured_events.is_empty(), "Took too long to capture any events");
+        assert!(captured_events.iter().any(|event| event["type"] == "update"));
+        let note_event = captured_events.iter().find(|event| event["type"] == "note").unwrap();
+        let note_data: serde_json::Value =
+            serde_json::from_str(note_event["data"].as_str().unwrap()).unwrap();
+        assert!(note_data["note_id"].is_string());
+        assert!(note_data["account_id"].is_string());
+        assert!(note_data["transaction_id"].is_string());
+        assert!(note_data["explorer_url"].is_string());
+
+        client.close().await.unwrap();
     }
 
     async fn start_test_faucet() -> Url {
@@ -461,6 +448,7 @@ mod test {
         .await
         .unwrap();
 
+        // Start the faucet connected to the stub
         // Use std::thread to launch faucet - avoids Send requirements
         std::thread::spawn(move || {
             // Create a new runtime for this thread
@@ -485,7 +473,7 @@ mod test {
         config.endpoint
     }
 
-    async fn start_chromedriver() -> String {
+    async fn start_fantoccini_client() -> fantoccini::Client {
         // Start chromedriver. This requires having chromedriver and chrome installed
         let chromedriver_port = "57708";
         let mut chromedriver = tokio::process::Command::new("chromedriver")
@@ -505,16 +493,13 @@ mod test {
                 break;
             }
         }
-        chromedriver_port.to_string()
-    }
 
-    async fn start_fantoccini_client(chromedriver_port: String) -> fantoccini::Client {
         // Start fantoccini client
         ClientBuilder::native()
             .capabilities(
                 [(
                     "goog:chromeOptions".to_string(),
-                    json!({"args": ["--disable-gpu", "--no-sandbox"]}),
+                    json!({"args": ["--headless", "--disable-gpu", "--no-sandbox"]}),
                 )]
                 .into_iter()
                 .collect::<Map<_, _>>(),

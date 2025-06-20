8d0c4451f49a048aa771c55fa3916524ce72d132
Bernhard Schuster <bernhard@ahoi.io>
2025-06-20 16:47:37 +0200

    setup database as first migration

--- crates/store/src/db/migrations.rs
+++ crates/store/src/db/migrations.rs
@@ -31,9 +31,13 @@ pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("src/db/migrations"
 pub fn apply_migrations(
     conn: &mut SqliteConnection,
 ) -> std::result::Result<(), crate::errors::DatabaseError> {
+    let migrations = conn.pending_migrations(MIGRATIONS).expect("In memory migrations never fail");
+    println!("Applying migrations: {}", migrations.len());
+
     let Err(e) = conn.run_pending_migrations(MIGRATIONS) else {
         return Ok(());
     };
+    eprintln!("Failed to apply migrations!");
     // something went wrong, MIGRATIONS contains
     conn.revert_last_migration(MIGRATIONS)
         .expect("Duality is maintained by the developer");

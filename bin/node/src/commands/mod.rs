a050160ec3ee953b2dc10bb2c820dde94c1ad64c
farrellh1 <farrell.hauzan@gmail.com>
2025-06-23 13:55:25 +0700

    refactor(workspace): add humantime dependency and update duration constants

--- bin/node/src/commands/mod.rs
+++ bin/node/src/commands/mod.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 pub mod block_producer;
 pub mod bundled;
 pub mod rpc;
@@ -15,11 +13,7 @@ const ENV_STORE_URL: &str = "MIDEN_NODE_STORE_URL";
 const ENV_DATA_DIRECTORY: &str = "MIDEN_NODE_DATA_DIRECTORY";
 const ENV_ENABLE_OTEL: &str = "MIDEN_NODE_ENABLE_OTEL";
 
-const DEFAULT_BLOCK_INTERVAL_MS: &str = "5000";
-const DEFAULT_BATCH_INTERVAL_MS: &str = "2000";
-const DEFAULT_MONITOR_INTERVAL_MS: &str = "10000";
-const DEFAULT_NTX_TICKER_INTERVAL_MS: &str = "200";
-
-fn parse_duration_ms(arg: &str) -> Result<std::time::Duration, std::num::ParseIntError> {
-    arg.parse().map(Duration::from_millis)
-}
+const DEFAULT_BLOCK_INTERVAL: &str = "5s";
+const DEFAULT_BATCH_INTERVAL: &str = "2s";
+const DEFAULT_MONITOR_INTERVAL: &str = "10s";
+const DEFAULT_NTX_TICKER_INTERVAL: &str = "200ms";

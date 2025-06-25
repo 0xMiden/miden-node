5f61ad8147a9edc23781493b0e1f879ef790d2f9
sergerad <sergerad@miden.team>
2025-06-25 15:02:32 +1200

    Update all format!() + as_report() to as_report_context() and replace remaining err.to_string() for error types

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -8,6 +8,7 @@ pub mod tracing;
 pub mod version;
 
 pub trait ErrorReport: std::error::Error {
+    /// Returns a string representation of the error and its source chain.
     fn as_report(&self) -> String {
         use std::fmt::Write;
         let mut report = self.to_string();
@@ -18,6 +19,44 @@ pub trait ErrorReport: std::error::Error {
 
         report
     }
+
+    /// Creates a new root in the error chain and returns a string representation of the error and
+    /// its source chain.
+    fn as_report_context(&self, context: &'static str) -> String {
+        format!("{context}: \nCaused by: {}", self.as_report())
+    }
 }
 
 impl<T: std::error::Error> ErrorReport for T {}
+
+#[cfg(test)]
+mod tests {
+    use crate::ErrorReport;
+
+    #[derive(thiserror::Error, Debug, Clone, PartialEq, Eq)]
+    pub enum TestSourceError {
+        #[error("Source error")]
+        Source,
+    }
+
+    #[derive(thiserror::Error, Debug)]
+    pub enum TestError {
+        #[error("Parent error")]
+        Parent(#[from] TestSourceError),
+    }
+
+    #[test]
+    fn as_report() {
+        let error = TestError::Parent(TestSourceError::Source);
+        assert_eq!("Parent error\nCaused by: Source error", error.as_report());
+    }
+
+    #[test]
+    fn as_report_context() {
+        let error = TestError::Parent(TestSourceError::Source);
+        assert_eq!(
+            "Final error: \nCaused by: Parent error\nCaused by: Source error",
+            error.as_report_context("Final error")
+        );
+    }
+}

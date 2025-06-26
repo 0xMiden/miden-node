798a3fa54e980c8886969ba9eb73c1f03c6b65fc
Bernhard Schuster <bernhard@ahoi.io>
2025-06-26 16:23:24 +0200

    chore: dir structure shuffle fix: make test useful

---
+++ crates/store/src/genesis/config/tests.rs
@@ -0,0 +1,16 @@
+use super::*;
+
+#[test]
+fn simple_01_works() -> Result<(), Box<dyn std::error::Error>> {
+    let s = include_str!("./samples/01-simple.toml");
+    let gcfg = TestGenesisConfig::read_toml(s)?;
+    let (state, _secrets) = gcfg.into_state()?;
+    let _ = state;
+    assert!(state.accounts[0].is_faucet());
+    assert!(state.accounts[1].is_regular_account());
+
+    assert_matches::assert_matches!(state.accounts[1].vault().get_balance(state.accounts[0].id()), Ok(val) => {
+        assert_eq!(val, 999);
+    });
+    Ok(())
+}

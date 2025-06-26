b3dea90ff27a3e6faa28d2dc71b09606ec5b6ad6
Bernhard Schuster <bernhard@ahoi.io>
2025-06-26 15:55:37 +0200

    fix: coherence
    Reduce functionality to `Basic*` implementations of `Faucet` for an account.

--- crates/store/src/genesis/config.rs
+++ crates/store/src/genesis/config.rs
@@ -1,20 +1,21 @@
 #![allow(dead_code)]
 #![allow(clippy::from_iter_instead_of_collect)]
+
 //! Describe a subset of the genesis manifest in easily human readable format
 
-use std::{collections::HashMap, path::Path};
+use std::collections::HashMap;
 
 use miden_lib::{
     AuthScheme,
-    account::{faucets::create_basic_fungible_faucet, wallets::create_basic_wallet},
-    utils::{self, Deserializable, ReadAdapter},
+    account::{auth::RpoFalcon512, faucets::BasicFungibleFaucet, wallets::create_basic_wallet},
+    utils::{self},
 };
 use miden_node_utils::crypto::get_rpo_random_coin;
 use miden_objects::{
     AccountError, AssetError, Felt, FieldElement, StarkField, TokenSymbolError,
     account::{
-        AccountDelta, AccountId, AccountIdAnchor, AccountStorageDelta, AccountStorageMode,
-        AccountType, AccountVaultDelta,
+        AccountBuilder, AccountDelta, AccountId, AccountIdAnchor, AccountStorageDelta,
+        AccountStorageMode, AccountType, AccountVaultDelta,
     },
     asset::{Asset, FungibleAsset, TokenSymbol},
     crypto::dsa::rpo_falcon512::SecretKey,
@@ -24,12 +25,14 @@ use rand_chacha::ChaCha20Rng;
 
 use crate::GenesisState;
 
+/// Represents an account, either a wallet or a faucet
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub enum AccountRepr {
     Wallet(WalletRepr),
     Faucet(FaucetRepr),
 }
 
+/// Represents a wallet, containing a set of assets
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct WalletRepr {
     can_be_updated: bool,
@@ -38,14 +41,21 @@ pub struct WalletRepr {
     assets: Vec<AssetEntry>,
 }
 
+/// `false` doesn't pass the `syn::Path` parsing, so we do one level indirection
+const fn nein() -> bool {
+    false
+}
+
+/// Represents a faucet with asset specific properties
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct FaucetRepr {
-    symbol: String, // limit to 3 charactesr
+    // TODO eventually directly parse to `TokenSymbol`
+    symbol: String,
     decimals: u8,
-    // TODO: unclear if we require storage_mode
-    max_supply: u64, // TODO is this sufficient? Serde has trouble using `u128`
+    max_supply: u64,
     #[serde(default)]
     storage_mode: StorageMode,
+    #[serde(default = "self::nein")]
     fungible: bool,
 }
 
@@ -54,18 +64,14 @@ pub struct FaucetRepr {
 #[derive(Debug, Clone, Copy, serde::Serialize, serde::Deserialize, Default)]
 pub enum StorageMode {
     /// Monitor for `Notes` related to the account, in addition to being `Public`.
-    #[serde(alias = "net")]
     #[serde(alias = "network")]
     #[default]
     Network,
     /// A publicly stored account, lives on-chain.
-    #[serde(alias = "public")]
     #[serde(alias = "pub")]
     Public,
     /// A private account, which must be known by interactors.
     #[serde(alias = "private")]
-    #[serde(alias = "sec")]
-    #[serde(alias = "secret")]
     Private,
 }
 
@@ -106,19 +112,36 @@ pub enum Error {
     TokenSymbol(#[from] TokenSymbolError),
     #[error("The provided max supply {max_supply} exceeds the field modulus {modulus}")]
     MaxSupplyExceedsFieldModulus { max_supply: u64, modulus: u64 },
+    #[error("Unsupported value for key {key} : {value}")]
+    UnsupportedValue {
+        key: &'static str,
+        value: String,
+        message: String,
+    },
 }
 
+/// Secrets generated during the state generation
 ///
+/// Attention: Only to be used for testing, for all other usecases
+/// inject only the public keys.
+#[derive(Debug, Clone)]
+pub struct TestSecrets {
+    pub secrets: Vec<(AccountType, AccountId, SecretKey)>,
+}
+
+/// Specify a set of faucets and wallets with assets for easier test depoyments
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-pub struct GenesisConfig {
+pub struct TestGenesisConfig {
     version: u32,
     timestamp: u32,
     account: Vec<WalletRepr>,
     faucet: Vec<FaucetRepr>,
 }
 
-impl GenesisConfig {
-    /// Read from a toml string
+impl TestGenesisConfig {
+    /// Read the genesis accounts from a toml formatted string
+    ///
+    /// Notice: It will generate the specified case during [`fn into_state`].
     pub fn read_toml(toml_str: &str) -> Result<Self, Error> {
         let me = toml::from_str::<Self>(toml_str)?;
         Ok(me)
@@ -128,64 +151,73 @@ impl GenesisConfig {
     ///
     /// Notice: Generates keys and returns the secret keys, hence this is not sane to be used
     /// for production environments. There, you want to generate the keys externally.
-    pub fn into_state(self) -> Result<GenesisState, Error> {
+    pub fn into_state(self) -> Result<(GenesisState, TestSecrets), Error> {
         let version = self.version;
         let timestamp = self.timestamp;
         let repr_accounts = self.account;
         let repr_faucets = self.faucet;
-        let mut accounts = Vec::new();
-        // TODO use `TokenSymbol` the symbol a type
+        let mut all_accounts = Vec::new();
+
+        // Every asset sitting in a wallet, has to reference a faucet for that asset
         let mut faucets = HashMap::<String, AccountId>::new();
 
-        // TODO
-        let _keys = HashMap::<Felt, SecretKey>::new();
-
-        fn load_serialized_lib_from_path<T: Deserializable>(path: &Path) -> Result<T, Error> {
-            let path = fs_err::canonicalize(path).map_err(Error::LibraryReference)?;
-            let mut f = fs_err::File::open(path).map_err(Error::LibraryReference)?;
-            let mut adapter = ReadAdapter::new(&mut f);
-            let lib = T::read_from(&mut adapter).map_err(Error::LibraryOnDiskFormat)?;
-            Ok(lib)
-        }
+        // Collect the generated secret keys for the test, so one can interact with those
+        // accounts/sign transactions
+        let mut secrets = Vec::<(AccountType, AccountId, SecretKey)>::new();
 
         let anchor = AccountIdAnchor::PRE_GENESIS;
         // first setup all the faucets
         for FaucetRepr {
             symbol,
-            decimals,   // TODO FIXME unused, but should be used
-            max_supply, // TODO FIXME unused, but should be used
+            decimals,
+            max_supply,
             storage_mode,
             fungible,
         } in repr_faucets
         {
             let mut rng = ChaCha20Rng::from_seed(rand::random());
             let secret = SecretKey::with_rng(&mut get_rpo_random_coin(&mut rng));
-            let auth = AuthScheme::RpoFalcon512 { pub_key: secret.public_key() };
+            let auth = RpoFalcon512::new(secret.public_key());
             let init_seed: [u8; 32] = rng.random();
 
             let token_symbol = TokenSymbol::new(&symbol)?;
             let max_supply = Felt::try_from(max_supply).map_err(|_| {
                 Error::MaxSupplyExceedsFieldModulus { max_supply, modulus: Felt::MODULUS }
             })?;
+
             let account_type = if fungible {
                 AccountType::FungibleFaucet
             } else {
-                todo!("We don't support non fungible ones just yet")
-                // AccountType::NonFungibleFaucet
+                AccountType::NonFungibleFaucet
             };
+
+            if !fungible {
+                return Err(Error::UnsupportedValue {
+                    key: "fungible",
+                    value: false.to_string(),
+                    message: "Not supported just yet".to_owned(),
+                });
+            }
+
+            let component = BasicFungibleFaucet::new(token_symbol, decimals, max_supply)
+                .map_err(AccountError::FungibleFaucetError)?;
+
             let account_storage_mode = storage_mode.into();
-            let (faucet_account, _seed) = create_basic_fungible_faucet(
-                init_seed,
-                anchor,
-                token_symbol,
-                decimals,
-                max_supply,
-                account_storage_mode,
-                auth,
-            )?;
+
+            // similar to `fn create_basic_fungible_faucet`, but we need to cover more cases
+            let (faucet_account, _faucet_account_seed) = AccountBuilder::new(init_seed)
+                .anchor(anchor)
+                .account_type(account_type)
+                .storage_mode(account_storage_mode)
+                .with_component(auth)
+                .with_component(component)
+                .build()?;
+
             faucets.insert(symbol, faucet_account.id());
 
-            accounts.push(faucet_account);
+            secrets.push((account_type, faucet_account.id(), secret));
+
+            all_accounts.push(faucet_account);
         }
 
         // then setup all wallet accounts, which reference the faucet's for their provided assets
@@ -222,9 +254,19 @@ impl GenesisConfig {
             )?;
             account.apply_delta(&delta)?;
 
-            accounts.push(account);
+            secrets.push((account_type, account.id(), secret));
+
+            all_accounts.push(account);
         }
-        Ok(GenesisState { accounts, version, timestamp })
+
+        Ok((
+            GenesisState {
+                accounts: all_accounts,
+                version,
+                timestamp,
+            },
+            TestSecrets { secrets },
+        ))
     }
 }
 
@@ -241,7 +283,7 @@ mod tests {
     #[test]
     fn simple_01_works() -> Result<(), Box<dyn std::error::Error>> {
         let s = include_str!("./samples/01-simple.toml");
-        let gcfg = GenesisConfig::read_toml(s)?;
+        let gcfg = TestGenesisConfig::read_toml(s)?;
         dbg!(&gcfg);
         let state = gcfg.into_state()?;
         let _ = dbg!(state);

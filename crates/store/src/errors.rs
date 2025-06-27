b56747d387289c86e734282252b3f93f0f2e085d
Bernhard Schuster <bernhard@ahoi.io>
2025-06-27 13:31:53 +0200

    improve error type

--- crates/store/src/errors.rs
+++ crates/store/src/errors.rs
@@ -76,6 +76,23 @@ pub enum DatabaseError {
     UnsupportedDatabaseVersion,
 }
 
+impl DatabaseError {
+    /// Converts from `InteractError`
+    ///
+    /// Note: Required since `InteractError` has at least one enum
+    /// variant that is _not_ `Send + Sync` and hence prevents the
+    /// `Sync` auto implementation.
+    /// This does an internal conversion to string while maintaining
+    /// convenience.
+    ///
+    /// Using `MSG` as const so it can be called as
+    /// `.map_err(DatabaseError::interact::<"Your message">)`
+    pub fn interact(msg: &(impl ToString + ?Sized), e: &InteractError) -> Self {
+        let msg = msg.to_string();
+        Self::InteractError(format!("{msg} failed: {e:?}"))
+    }
+}
+
 impl From<DatabaseError> for Status {
     fn from(err: DatabaseError) -> Self {
         match err {
@@ -140,7 +157,10 @@ pub enum InvalidBlockError {
     #[error("received invalid account tree root")]
     NewBlockInvalidAccountRoot,
     #[error("new block number must be 1 greater than the current block number")]
-    NewBlockInvalidBlockNum,
+    NewBlockInvalidBlockNum {
+        expected: BlockNumber,
+        submitted: BlockNumber,
+    },
     #[error("new block chain commitment is not consistent with chain MMR")]
     NewBlockInvalidChainCommitment,
     #[error("received invalid note root")]

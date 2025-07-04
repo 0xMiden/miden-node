79d2e6c6571ff98b7e0deb10884205386984b211
Bernhard Schuster <bernhard@ahoi.io>
2025-07-04 11:56:52 +0200

    lowercase

--- crates/store/src/genesis/config/errors.rs
+++ crates/store/src/genesis/config/errors.rs
@@ -6,24 +6,24 @@ use miden_objects::{AccountError, AssetError, TokenSymbolError, asset::TokenSymb
 pub enum Error {
     #[error(transparent)]
     Toml(#[from] toml::de::Error),
-    #[error("Account translation from config to state failed")]
+    #[error("account translation from config to state failed")]
     Account(#[from] AccountError),
-    #[error("Asset translation from config to state failed")]
+    #[error("asset translation from config to state failed")]
     Asset(#[from] AssetError),
-    #[error("Applying assets to account failed")]
+    #[error("applying assets to account failed")]
     AccountDelta(#[from] miden_objects::AccountDeltaError),
-    #[error("The defined asset {symbol:?} has no corresponding faucet")]
+    #[error("the defined asset {symbol:?} has no corresponding faucet")]
     MissingFaucetDefinition { symbol: TokenSymbol },
     #[error(transparent)]
     TokenSymbol(#[from] TokenSymbolError),
-    #[error("The provided max supply {max_supply} exceeds the field modulus {modulus}")]
+    #[error("the provided max supply {max_supply} exceeds the field modulus {modulus}")]
     MaxSupplyExceedsFieldModulus { max_supply: u64, modulus: u64 },
-    #[error("Unsupported value for key {key} : {value}")]
+    #[error("unsupported value for key {key} : {value}")]
     UnsupportedValue {
         key: &'static str,
         value: String,
         message: String,
     },
-    #[error("Failed to create fungible faucet account")]
+    #[error("failed to create fungible faucet account")]
     FungibleFaucet(#[from] FungibleFaucetError),
 }

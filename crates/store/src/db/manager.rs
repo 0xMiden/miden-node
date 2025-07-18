ee039a059404fd120ac8ba1aac9adb00990a0ffc
Bernhard Schuster <bernhard@ahoi.io>
2025-07-18 15:24:46 +0200

    cleanup stray changes

--- crates/store/src/db/manager.rs
+++ crates/store/src/db/manager.rs
@@ -2,8 +2,43 @@
 //!
 //! Only required to setup connection parameters, specifically `WAL`.
 
-use super::{DatabaseError, Result, RunQueryDsl, SqliteConnection};
+use deadpool_sync::InteractError;
 
+use super::{Result, RunQueryDsl, SqliteConnection};
+
+#[derive(thiserror::Error, Debug)]
+pub enum ConnManError {
+    #[error("failed to apply connection parameter")]
+    ConnectionParamSetup(#[source] diesel::result::Error),
+    #[error("SQLite pool interaction failed: {0}")]
+    InteractError(String),
+    #[error("failed to create a new connection")]
+    ConnectionCreate(#[source] deadpool_diesel::Error),
+    #[error("failed to recycle connection")]
+    PoolRecycle(#[source] deadpool::managed::RecycleError<deadpool_diesel::Error>),
+}
+
+impl ConnManError {
+    /// Converts from `InteractError`
+    ///
+    /// Note: Required since `InteractError` has at least one enum
+    /// variant that is _not_ `Send + Sync` and hence prevents the
+    /// `Sync` auto implementation.
+    /// This does an internal conversion to string while maintaining
+    /// convenience.
+    ///
+    /// Using `MSG` as const so it can be called as
+    /// `.map_err(DatabaseError::interact::<"Your message">)`
+    pub fn interact(msg: &(impl ToString + ?Sized), e: &InteractError) -> Self {
+        let msg = msg.to_string();
+        Self::InteractError(format!("{msg} failed: {e:?}"))
+    }
+}
+
+/// Create a connection manager with per-connection setup
+///
+/// Particularly, `foreign_key` checks are enabled and using
+/// a write-append-log for journaling.
 pub(crate) struct WalConnManager {
     pub(crate) manager: deadpool_diesel::sqlite::Manager,
 }
@@ -18,17 +53,16 @@ impl WalConnManager {
     }
 }
 
-// FIXME use a better error type
 impl deadpool::managed::Manager for WalConnManager {
     type Type = deadpool_sync::SyncWrapper<SqliteConnection>;
-    type Error = DatabaseError;
+    type Error = ConnManError;
 
     async fn create(&self) -> Result<Self::Type, Self::Error> {
-        let conn = self.manager.create().await?;
+        let conn = self.manager.create().await.map_err(ConnManError::ConnectionCreate)?;
 
         conn.interact(configure_connection_on_creation)
             .await
-            .map_err(|e| DatabaseError::interact("Connection setup", &e))??;
+            .map_err(|e| ConnManError::interact("Connection setup", &e))??;
         Ok(conn)
     }
 
@@ -37,21 +71,27 @@ impl deadpool::managed::Manager for WalConnManager {
         conn: &mut Self::Type,
         metrics: &deadpool_diesel::Metrics,
     ) -> deadpool::managed::RecycleResult<Self::Error> {
-        self.manager.recycle(conn, metrics).await.map_err(DatabaseError::PoolRecycle)?;
+        self.manager.recycle(conn, metrics).await.map_err(|err| {
+            deadpool::managed::RecycleError::Backend(ConnManError::PoolRecycle(err))
+        })?;
         Ok(())
     }
 }
 
 pub(crate) fn configure_connection_on_creation(
     conn: &mut SqliteConnection,
-) -> Result<(), DatabaseError> {
+) -> Result<(), ConnManError> {
     // Enable the WAL mode. This allows concurrent reads while the
     // transaction is being written, this is required for proper
     // synchronization of the servers in-memory and on-disk representations
     // (see [State::apply_block])
-    diesel::sql_query("PRAGMA journal_mode=WAL").execute(conn)?;
+    diesel::sql_query("PRAGMA journal_mode=WAL")
+        .execute(conn)
+        .map_err(ConnManError::ConnectionParamSetup)?;
 
     // Enable foreign key checks.
-    diesel::sql_query("PRAGMA foreign_keys=ON").execute(conn)?;
+    diesel::sql_query("PRAGMA foreign_keys=ON")
+        .execute(conn)
+        .map_err(ConnManError::ConnectionParamSetup)?;
     Ok(())
 }

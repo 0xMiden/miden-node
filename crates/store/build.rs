d8b2d73d67ae76f99dfb68c513420f74db5de553
Bernhard Schuster <bernhard@ahoi.io>
2025-07-22 09:48:49 +0200

    do not re-run on src changes

--- crates/store/build.rs
+++ crates/store/build.rs
@@ -3,9 +3,6 @@ fn main() {
     println!("cargo:rerun-if-changed=./src/db/migrations");
     // if we do one re-write, the defaults are gone
     // <https://doc.rust-lang.org/cargo/reference/build-scripts.html#rerun-if-changed>
-    // TODO in our case migrations are part of the source tree, investigate if this file is needed
-    // TODO at all
     println!("cargo:rerun-if-changed=Cargo.toml");
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=src");
 }

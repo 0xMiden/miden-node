ad28472ebfa8b3b2bcd309c162b6fa07100bf03f
sergerad <sergerad@miden.team>
2025-07-24 14:09:15 +1200

    Reinstate chain_tip in ntx state

--- crates/ntx-builder/src/state/mod.rs
+++ crates/ntx-builder/src/state/mod.rs
@@ -11,8 +11,9 @@ use miden_node_proto::domain::{
 use miden_node_utils::tracing::OpenTelemetrySpanExt;
 use miden_objects::{
     account::{Account, delta::AccountUpdateDetails},
+    block::{BlockHeader, BlockNumber},
     note::Nullifier,
-    transaction::TransactionId,
+    transaction::{PartialBlockchain, TransactionId},
 };
 use tracing::instrument;
 
@@ -30,14 +31,30 @@ mod account;
 pub struct TransactionCandidate {
     /// The current inflight state of the account.
     pub account: Account,
+
     /// A set of notes addressed to this network account.
     pub notes: Vec<NetworkNote>,
+
+    /// The latest locally committed block header.
+    ///
+    /// This should be used as the reference block during transaction execution. It is guaranteed
+    /// to be tracked by `chain_mmr`.
+    pub chain_tip: BlockHeader,
+
+    /// The chain MMR, which includes the `chain_tip`'s block.
+    pub chain_mmr: PartialBlockchain,
 }
 
 /// Holds the state of the network transaction builder.
 ///
 /// It tracks inflight transactions, and their impact on network-related state.
 pub struct State {
+    /// The latest committed block header.
+    chain_tip: BlockHeader,
+
+    /// The chain MMR including the latest block header.
+    chain_mmr: PartialBlockchain,
+
     /// Tracks all network accounts with inflight state.
     ///
     /// This is network account deltas, network notes and their nullifiers.
@@ -73,7 +90,16 @@ impl State {
     /// Load's all available network notes from the store, along with the required account states.
     #[instrument(target = COMPONENT, name = "ntx.state.load", skip_all)]
     pub async fn load(store: StoreClient) -> Result<Self, StoreError> {
+        let (chain_tip, chain_mmr) = store
+            .get_latest_blockchain_data_with_retry()
+            .await?
+            .expect("store should contain a latest block");
+
+        let chain_mmr = PartialBlockchain::new(chain_mmr, [])
+            .expect("PartialBlockchain should build from latest partial MMR");
         let mut state = Self {
+            chain_tip,
+            chain_mmr,
             store,
             accounts: HashMap::default(),
             queue: VecDeque::default(),
@@ -146,11 +172,22 @@ impl State {
             }
 
             self.in_progress.insert(candidate);
-            return TransactionCandidate { account: account.latest_account(), notes }.into();
+            return TransactionCandidate {
+                account: account.latest_account(),
+                notes,
+                chain_tip: self.chain_tip.clone(),
+                chain_mmr: self.chain_mmr.clone(),
+            }
+            .into();
         }
         self.inject_telemetry();
 
         None
+    }
+
+    /// The latest block number the state knows of.
+    pub fn chain_tip(&self) -> BlockNumber {
+        self.chain_tip.block_num()
     }
 
     /// Marks a previously selected candidate account as failed, allowing it to be available for
@@ -178,7 +215,17 @@ impl State {
             } => {
                 self.add_transaction(id, nullifiers, network_notes, account_delta).await?;
             },
-            MempoolEvent::BlockCommitted { header: _, txs } => {
+            MempoolEvent::BlockCommitted { header, txs } => {
+                anyhow::ensure!(
+                    header.prev_block_commitment() == self.chain_tip.commitment(),
+                    "New block's parent commitment {} does not match local chain tip {}",
+                    header.prev_block_commitment(),
+                    self.chain_tip.commitment()
+                );
+
+                // Chain MMR always lags by one block.
+                self.chain_mmr.add_block(self.chain_tip.clone(), true);
+                self.chain_tip = header;
                 for tx in txs {
                     self.commit_transaction(tx);
                 }

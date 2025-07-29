bbd382b6ce61c673acc32632e44458a24f7680a5
Bernhard Schuster <bernhard@ahoi.io>
2025-07-29 15:38:03 +0200

    better doc in build.rs

--- crates/store/build.rs
+++ crates/store/build.rs
@@ -1,5 +1,9 @@
 fn main() {
     // trigger a rebuild if any migrations change
+    // such that we include the latest version and not use a cached
+    // version using `diesel_migrations::embed_migrations!(dir)`
+    // It's an implementation detail:
+    // <https://docs.rs/diesel_migrations/2.2.0/diesel_migrations/macro.embed_migrations.html>
     println!("cargo:rerun-if-changed=./src/db/migrations");
     // if we do one re-write, the defaults are gone
     // <https://doc.rust-lang.org/cargo/reference/build-scripts.html#rerun-if-changed>

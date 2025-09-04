bab3de60e95533b6d4e68dc2b49a9559fc49b20e
SantiagoPittella <pittellasantiago@gmail.com>
2025-09-04 13:20:22 -0300

    review: further simplification of grpc_spans

--- crates/utils/src/tracing/grpc.rs
+++ crates/utils/src/tracing/grpc.rs
@@ -1,115 +1,26 @@
-/// Represents an Otel compatible, traced Miden component.
-///
-/// Used to select an appropriate service name for the tonic server to use.
-#[derive(Copy, Clone)]
-pub enum TracedComponent {
-    Rpc,
-    BlockProducer,
-    StoreRpc,
-    StoreBlockProducer,
-    StoreNtxBuilder,
-    RemoteProver,
-    RemoteProverProxy,
-}
+use crate::tracing::OpenTelemetrySpanExt;
 
-impl TracedComponent {
-    /// Returns the service name for this component.
-    fn service_name(self) -> &'static str {
-        match self {
-            TracedComponent::Rpc => "rpc.rpc",
-            TracedComponent::BlockProducer => "block-producer.rpc",
-            TracedComponent::StoreRpc => "store.rpc.rpc",
-            TracedComponent::StoreBlockProducer => "store.block-producer.rpc",
-            TracedComponent::StoreNtxBuilder => "store.ntx-builder.rpc",
-            TracedComponent::RemoteProver => "remote-prover.rpc",
-            TracedComponent::RemoteProverProxy => "remote-prover-proxy.rpc",
-        }
-    }
-}
-
-/// Returns a [`trace_fn`](tonic::transport::server::Server) implementation for the RPC which
-/// adds open-telemetry information to the span.
+/// Returns a [`trace_fn`](tonic::transport::server::Server) implementation for gRPC requests
+/// which adds open-telemetry information to the span.
 ///
-/// Creates an `info` span following the open-telemetry standard: `{service}.rpc/{method}`.
+/// Creates an `info` span following the open-telemetry standard: `{service}/{method}`.
 /// The span name is dynamically set using the HTTP path via the `otel.name` field.
 /// Additionally also pulls in remote tracing context which allows the server trace to be connected
 /// to the client's origin trace.
-pub fn traced_span_fn<T>(component: TracedComponent) -> fn(&http::Request<T>) -> tracing::Span {
-    match component {
-        TracedComponent::Rpc => rpc_trace_fn,
-        TracedComponent::BlockProducer => block_producer_trace_fn,
-        TracedComponent::StoreRpc => store_rpc_trace_fn,
-        TracedComponent::StoreBlockProducer => store_block_producer_trace_fn,
-        TracedComponent::StoreNtxBuilder => store_ntx_builder_trace_fn,
-        TracedComponent::RemoteProver => remote_prover_trace_fn,
-        TracedComponent::RemoteProverProxy => remote_prover_proxy_trace_fn,
-    }
-}
-
-/// Creates a dynamic tracing span based on the HTTP path of the gRPC request.
-///
-/// This function extracts the method name from the HTTP path and creates a span with
-/// the service name from the component. The actual span name used by OpenTelemetry
-/// is set dynamically via the `otel.name` field, allowing us to avoid hardcoded
-/// method name matches.
-fn dynamic_trace_fn<T>(request: &http::Request<T>, component: TracedComponent) -> tracing::Span {
-    let service_name = component.service_name();
-
-    // Extract the method name from the HTTP path (e.g., "/rpc.rpc/CheckNullifiers" ->
-    // "CheckNullifiers")
-    let method_name = request.uri().path().rsplit('/').next().unwrap_or("Unknown");
+pub fn grpc_trace_fn<T>(request: &http::Request<T>) -> tracing::Span {
+    // A gRPC request's path ends with `../<service>/<method>`.
+    let mut path_segments = request.uri().path().rsplit('/');
+    let method = path_segments.next().unwrap_or_default();
+    let service = path_segments.next().unwrap_or_default();
 
     // Create a span with a generic name - the actual span name will be set via otel.name
-    let span =
-        tracing::info_span!("grpc_request", rpc.service = service_name, rpc.method = method_name);
+    let span = tracing::info_span!("grpc_request", rpc.service = service, rpc.method = method);
 
     // Set the dynamic span name that OpenTelemetry will use
     // This allows us to have the proper <service>/<method> format without hardcoding
-    span.record("otel.name", format!("{service_name}/{method_name}"));
+    span.record("otel.name", format!("{service}/{method}"));
 
-    // Add OpenTelemetry context and network attributes
-    let span = add_otel_span_attributes(span, request);
-    add_network_attributes(span, request)
-}
-
-// Individual trace functions that delegate to the dynamic implementation
-fn rpc_trace_fn<T>(request: &http::Request<T>) -> tracing::Span {
-    dynamic_trace_fn(request, TracedComponent::Rpc)
-}
-
-fn block_producer_trace_fn<T>(request: &http::Request<T>) -> tracing::Span {
-    dynamic_trace_fn(request, TracedComponent::BlockProducer)
-}
-
-fn store_rpc_trace_fn<T>(request: &http::Request<T>) -> tracing::Span {
-    dynamic_trace_fn(request, TracedComponent::StoreRpc)
-}
-
-fn store_block_producer_trace_fn<T>(request: &http::Request<T>) -> tracing::Span {
-    dynamic_trace_fn(request, TracedComponent::StoreBlockProducer)
-}
-
-fn store_ntx_builder_trace_fn<T>(request: &http::Request<T>) -> tracing::Span {
-    dynamic_trace_fn(request, TracedComponent::StoreNtxBuilder)
-}
-
-fn remote_prover_trace_fn<T>(request: &http::Request<T>) -> tracing::Span {
-    dynamic_trace_fn(request, TracedComponent::RemoteProver)
-}
-
-fn remote_prover_proxy_trace_fn<T>(request: &http::Request<T>) -> tracing::Span {
-    dynamic_trace_fn(request, TracedComponent::RemoteProverProxy)
-}
-
-/// Adds remote tracing context to the span.
-///
-/// Could be expanded in the future by adding in more open-telemetry properties.
-fn add_otel_span_attributes<T>(span: tracing::Span, request: &http::Request<T>) -> tracing::Span {
-    // Pull the open-telemetry parent context using the HTTP extractor. We could make a more
-    // generic gRPC extractor by utilising the gRPC metadata. However that
-    //     (a) requires cloning headers,
-    //     (b) we would have to write this ourselves, and
-    //     (c) gRPC metadata is transferred using HTTP headers in any case.
+    // Pull the open-telemetry parent context using the HTTP extractor
     let otel_ctx = opentelemetry::global::get_text_map_propagator(|propagator| {
         propagator.extract(&MetadataExtractor(&tonic::metadata::MetadataMap::from_headers(
             request.headers().clone(),
@@ -117,14 +28,10 @@ fn add_otel_span_attributes<T>(span: tracing::Span, request: &http::Request<T>) 
     });
     tracing_opentelemetry::OpenTelemetrySpanExt::set_parent(&span, otel_ctx);
 
-    span
-}
+    // Adds various network attributes to the span, including remote address and port.
+    //
+    // See [server attributes](https://opentelemetry.io/docs/specs/semconv/rpc/rpc-spans/#server-attributes).
 
-/// Adds various network attributes to the span, including remote address and port.
-///
-/// See [server attributes](https://opentelemetry.io/docs/specs/semconv/rpc/rpc-spans/#server-attributes).
-fn add_network_attributes<T>(span: tracing::Span, request: &http::Request<T>) -> tracing::Span {
-    use super::OpenTelemetrySpanExt;
     // Set HTTP attributes.
     span.set_attribute("rpc.system", "grpc");
     if let Some(host) = request.uri().host() {

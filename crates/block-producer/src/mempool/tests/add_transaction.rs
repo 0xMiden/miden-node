e6bce41ecd921c78c4974ba8d07c82206cd783f8
Mirko <48352201+Mirko-von-Leipzig@users.noreply.github.com>
2025-10-03 13:37:38 +0200

    tx expiration checks

--- crates/block-producer/src/mempool/tests/add_transaction.rs
+++ crates/block-producer/src/mempool/tests/add_transaction.rs
@@ -11,45 +11,74 @@ use crate::test_utils::MockProvenTxBuilder;
 
 /// Ensures that transactions that expire before or within the expiration slack of the chain tip
 /// are rejected.
-#[test]
-fn expiration_slack_is_upheld() {
-    let (mut uut, _) = Mempool::for_tests();
-    assert!(
-        uut.config.expiration_slack > 0,
-        "Test is only useful if there is some expiration slack"
-    );
+mod tx_expiration {
+    use super::*;
 
-    // Create at least one block so that we can test an already expired tx as well.
-    let (number, _) = uut.select_block();
-    let header = BlockHeader::mock(number, None, None, &[], Word::default());
-    uut.commit_block(header);
+    fn setup() -> Mempool {
+        let (mut uut, _) = Mempool::for_tests();
+        assert!(
+            uut.config.expiration_slack > 0,
+            "Test is only useful if there is some expiration slack"
+        );
 
-    let from = uut.chain_tip.parent().expect("we have at least one block");
-    let limit = uut.chain_tip + uut.config.expiration_slack;
+        // Create at least some locally retained state.
+        let slack = uut.config.expiration_slack;
+        for _ in 0..slack + 10 {
+            let (number, _) = uut.select_block();
+            let header = BlockHeader::mock(number, None, None, &[], Word::default());
+            uut.commit_block(header);
+        }
 
-    for i in from.as_u32()..=limit.as_u32() {
+        uut
+    }
+
+    #[test]
+    fn expiration_after_slack_limit_is_accepted() {
+        let mut uut = setup();
+        let limit = uut.chain_tip + uut.config.expiration_slack;
+
         let tx = MockProvenTxBuilder::with_account_index(0)
-            .expiration_block_num(i.into())
+            .expiration_block_num(limit.child())
             .build();
         let tx = AuthenticatedTransaction::from_inner(tx).with_authentication_height(uut.chain_tip);
         let tx = Arc::new(tx);
-
-        let result = uut.add_transaction(tx);
-
-        assert_matches!(
-            result,
-            Err(AddTransactionError::Expired { .. }),
-            "Failed run with expiration {i} and limit {limit}"
-        );
+        uut.add_transaction(tx).unwrap();
     }
 
-    // Test that it passes if its beyond the limit.
-    let tx = MockProvenTxBuilder::with_account_index(0)
-        .expiration_block_num(limit.child())
-        .build();
-    let tx = AuthenticatedTransaction::from_inner(tx).with_authentication_height(uut.chain_tip);
-    let tx = Arc::new(tx);
-    uut.add_transaction(tx).unwrap();
+    #[test]
+    fn expiration_within_slack_limit_is_rejected() {
+        let mut uut = setup();
+        let limit = uut.chain_tip + uut.config.expiration_slack;
+
+        for i in uut.chain_tip.child().as_u32()..=limit.as_u32() {
+            let tx = MockProvenTxBuilder::with_account_index(0)
+                .expiration_block_num(i.into())
+                .build();
+            let tx =
+                AuthenticatedTransaction::from_inner(tx).with_authentication_height(uut.chain_tip);
+            let tx = Arc::new(tx);
+            let result = uut.add_transaction(tx);
+
+            assert_matches!(
+                result,
+                Err(AddTransactionError::Expired { .. }),
+                "Failed run with expiration {i} and limit {limit}"
+            );
+        }
+    }
+
+    #[test]
+    fn already_expired_is_rejected() {
+        let mut uut = setup();
+        let tx = MockProvenTxBuilder::with_account_index(0)
+            .expiration_block_num(uut.chain_tip)
+            .build();
+        let tx = AuthenticatedTransaction::from_inner(tx).with_authentication_height(uut.chain_tip);
+        let tx = Arc::new(tx);
+        let result = uut.add_transaction(tx);
+
+        assert_matches!(result, Err(AddTransactionError::Expired { .. }));
+    }
 }
 
 /// Ensures that a transaction's authentication height is always within the overlap between the

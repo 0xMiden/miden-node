9881d3dfc1730fae3657f12cdc19563ef233ba11
sergerad <sergerad@miden.team>
2025-10-15 13:36:55 +1300

    server scaffolding done

--- crates/validator/src/server/mod.rs
+++ crates/validator/src/server/mod.rs
@@ -1,6 +1,17 @@
 use std::net::SocketAddr;
 use std::time::Duration;
 
+use anyhow::Context;
+use miden_node_proto::generated::validator::api_server;
+use miden_node_proto::generated::{self as proto};
+use miden_node_proto_build::validator_api_descriptor;
+use miden_node_utils::panic::catch_panic_layer_fn;
+use miden_node_utils::tracing::grpc::grpc_trace_fn;
+use tokio::net::TcpListener;
+use tokio_stream::wrappers::TcpListenerStream;
+use tower_http::catch_panic::CatchPanicLayer;
+use tower_http::trace::TraceLayer;
+
 use crate::COMPONENT;
 
 pub struct Validator {
@@ -17,9 +28,56 @@ impl Validator {
     ///
     /// Executes in place (i.e. not spawned) and will run indefinitely until a fatal error is
     /// encountered.
-    #[allow(clippy::too_many_lines)]
     pub async fn serve(self) -> anyhow::Result<()> {
         tracing::info!(target: COMPONENT, endpoint=?self.address, "Initializing server");
+
+        let listener = TcpListener::bind(self.address)
+            .await
+            .context("failed to bind to block producer address")?;
+
+        let reflection_service = tonic_reflection::server::Builder::configure()
+            .register_file_descriptor_set(validator_api_descriptor())
+            .build_v1()
+            .context("failed to build reflection service")?;
+
+        // This is currently required for postman to work properly because
+        // it doesn't support the new version yet.
+        //
+        // See: <https://github.com/postmanlabs/postman-app-support/issues/13120>.
+        let reflection_service_alpha = tonic_reflection::server::Builder::configure()
+            .register_file_descriptor_set(validator_api_descriptor())
+            .build_v1alpha()
+            .context("failed to build reflection service")?;
+
+        // Build the gRPC server with the API service and trace layer.
+        tonic::transport::Server::builder()
+            .layer(CatchPanicLayer::custom(catch_panic_layer_fn))
+            .layer(TraceLayer::new_for_grpc().make_span_with(grpc_trace_fn))
+            .timeout(self.grpc_timeout)
+            .add_service(api_server::ApiServer::new(self))
+            .add_service(reflection_service)
+            .add_service(reflection_service_alpha)
+            .serve_with_incoming(TcpListenerStream::new(listener))
+            .await
+            .context("failed to serve block producer API")
+    }
+}
+
+#[tonic::async_trait]
+impl api_server::Api for Validator {
+    /// ...
+    async fn status(
+        &self,
+        request: tonic::Request<()>,
+    ) -> Result<tonic::Response<proto::validator::ValidatorStatus>, tonic::Status> {
+        todo!()
+    }
+
+    /// ...
+    async fn submit_proven_transaction(
+        &self,
+        request: tonic::Request<proto::transaction::ProvenTransaction>,
+    ) -> Result<tonic::Response<()>, tonic::Status> {
         todo!()
     }
 }

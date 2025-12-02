5fac6b96b77fbac1bbb776392abcfa3772929857
Mirko <48352201+Mirko-von-Leipzig@users.noreply.github.com>
2025-12-02 09:13:20 +0200

    fix pruning

--- scripts/reflow.sh
+++ scripts/reflow.sh
@@ -1,4 +1,4 @@
-find . -name target -prune -name generated -prune -o -type f -name "*.rs" -print0 |
+find . \( -name target -prune -o -name generated -prune \) -o -type f -name "*.rs" -print0 |
   while read -r -d $'\0' x; do
     awk -i inplace -f scripts/reflow.awk "$x"
   done

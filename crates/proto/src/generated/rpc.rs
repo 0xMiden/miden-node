d459a7e241227a2fc037d88d295d82f07a57cd10
Bernhard Schuster <bernhard@ahoi.io>
2025-12-18 21:42:15 +0100

    indent

--- crates/proto/src/generated/rpc.rs
+++ crates/proto/src/generated/rpc.rs
@@ -665,7 +665,7 @@ pub mod api_client {
         ///
         /// * `empty_leaf_index`: Non-inclusion proof (nullifier not in tree)
         /// * `single` or `multiple`: Inclusion proof only if the requested nullifier appears as a key.
-        ///  The value associated with the nullifier key represents the block number at which it was consumed.
+        ///   The value associated with the nullifier key represents the block number at which it was consumed.
         ///
         /// Verify proofs against the nullifier tree root in the latest block header.
         pub async fn check_nullifiers(

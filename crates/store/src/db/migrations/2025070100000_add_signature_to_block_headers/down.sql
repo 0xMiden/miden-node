a392c3d23a557178b5a5830ee1207e25d12f9fa8
sergerad <sergerad@miden.team>
2025-12-22 10:53:20 +1300

    RM migrations

--- crates/store/src/db/migrations/2025070100000_add_signature_to_block_headers/down.sql
+++
@@ -1,2 +0,0 @@
--- Remove signature column from block_headers table
-ALTER TABLE block_headers DROP COLUMN signature;
--- crates/store/src/db/migrations/2025070100000_add_signature_to_block_headers/up.sql
+++
@@ -1,5 +0,0 @@
--- Add signature column to block_headers table
-ALTER TABLE block_headers ADD COLUMN signature BLOB NOT NULL DEFAULT '';
-
--- Update existing rows to have empty signature (will be populated later if needed)
--- The default empty blob will be used for existing entries

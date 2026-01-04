4016a7907327817fedbfef68e4919adeadb7583c
sergerad <sergerad@miden.team>
2026-01-05 09:50:24 +1300

    RM unnecessary clone

--- crates/validator/src/server/mod.rs
+++ crates/validator/src/server/mod.rs
@@ -143,7 +143,7 @@ impl<S: BlockSigner + Send + Sync + 'static> api_server::Api for ValidatorServer
 
         // Validate the transaction.
         let validated_tx_header =
-            validate_transaction(proven_tx.clone(), tx_inputs).await.map_err(|err| {
+            validate_transaction(proven_tx, tx_inputs).await.map_err(|err| {
                 Status::invalid_argument(err.as_report_context("Invalid transaction"))
             })?;
 

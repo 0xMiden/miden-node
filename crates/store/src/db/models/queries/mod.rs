ab32f16133e67b062de528f09bb1cf47071e1af8
Bernhard Schuster <bernhard@ahoi.io>
2026-01-19 16:45:01 +0100

    Revert "paged loading of nullifiers and account commitments"
    This reverts commit 8145852444e9c4cb171eccebe19367bb10a3b1d3.

--- crates/store/src/db/models/queries/mod.rs
+++ crates/store/src/db/models/queries/mod.rs
@@ -47,7 +47,6 @@ pub use block_headers::*;
 mod accounts;
 pub use accounts::*;
 mod nullifiers;
-pub use nullifiers::NullifiersPage;
 pub(crate) use nullifiers::*;
 mod notes;
 pub(crate) use notes::*;

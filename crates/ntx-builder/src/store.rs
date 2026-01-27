22c47c07d48273e9391671554d59cf39fe47d6ff
SantiagoPittella <pittellasantiago@gmail.com>
2026-01-27 10:34:47 -0300

    chore: replace BlockNumber::from(0) with ::GENESIS

--- crates/ntx-builder/src/store.rs
+++ crates/ntx-builder/src/store.rs
@@ -236,7 +236,7 @@ impl StoreClient {
         &self,
         sender: tokio::sync::mpsc::Sender<NetworkAccountId>,
     ) -> Result<(), StoreError> {
-        let mut block_range = BlockNumber::from(0)..=BlockNumber::MAX;
+        let mut block_range = BlockNumber::GENESIS..=BlockNumber::MAX;
 
         while let Some(next_start) = self.load_accounts_page(block_range, &sender).await? {
             block_range = next_start..=BlockNumber::MAX;

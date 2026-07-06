9c8bd360597e506cd09201e988a10aa7b302cc5b
sergerad <sergerad@miden.team>
2026-07-07 09:41:27 +1200

    Add test line

--- bin/ntx-builder/src/main.rs
+++ bin/ntx-builder/src/main.rs
@@ -3,6 +3,7 @@ mod commands;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    println!("testing docker cache");
     let command = commands::NtxBuilderCommand::parse();
 
     let _otel_guard = miden_node_utils::logging::setup_tracing(command.open_telemetry())?;

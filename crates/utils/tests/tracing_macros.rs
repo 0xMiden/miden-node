53dd57e05c3a39ea52b44a7b7f19a2741c02cbff
KOVACS Krisztian <krisztian.k@miden.team>
2026-07-29 15:40:52 +0200

    fix(tracing): validate instrument field names (#2399)

--- crates/utils/tests/tracing_macros.rs
+++ crates/utils/tests/tracing_macros.rs
@@ -179,5 +179,6 @@ fn ui_tests() {
     let tests = trybuild::TestCases::new();
     tests.pass("tests/ui/tracing_macros/pass.rs");
     tests.compile_fail("tests/ui/tracing_macros/invalid_field_name.rs");
+    tests.compile_fail("tests/ui/tracing_macros/invalid_instrument_field_name.rs");
     tests.compile_fail("tests/ui/tracing_macros/outside_miden_instrument.rs");
 }
